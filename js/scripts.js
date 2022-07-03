32b1503957c6348f50a99f4a311d4e735b1f692c
Sárdi András <sardi.andras@gmail.com>
2022-07-03 18:32:34 +0200

    classrace

--- js/scripts.js
+++ js/scripts.js
@@ -7,6 +7,7 @@ const generatednames = ["1","2","3","4","5","6","7","8","9","10"];
 const dndclass = ["Artificer","Barbarian","Bard","Blood Hunter","Cleric","Druid","Fighter","Monk","Paladin","Ranger","Rogue","Sorcerer","Warlock","Wizard"];
 const dndrace = ["Dragonborn","Dwarf","Elf","Gnome","Half-Elf","Half-Orc","Halfling","Human","Tiefling","Aarakocra","Aasimar","Fairy","Firbolg","Genasi","Gith","Goliath","Harengon","Kenku","Locathah","Owlin","Tabaxi","Tortle","Triton","Verdan","Bugbear","Centaur","Goblin","Grung","Hobgoblin","Kobold","Lizardfolk","Minotaur","Orc","Yuan-Ti","Pureblood","Autognome","Dhamphir","Giff","Hadozee","Hexblood","Fairy","Kender","Owlfolk","Plasmoid","Rabbitfolk","Reborn","Revenant","Thri-Kreen","Eberron","Changeling","Kalashtar","Shifter","Warforged","Aetherborn","Aven","Khenra","Kor","Merfolk","Naga","Siren","Vampire","Dhamphir","Hexblood","Reborn","Ravnica","Loxodon","Simic","Hybrid","Vedalken","Viashino","Theros","Leonin","Satyr"];
 
+
 function generatecharacter() {
     let generatedclass =dndclass[generateRandomInteger(dndclass.length)-1];
     let generatedrace =dndrace[generateRandomInteger(dndrace.length)-1];
@@ -22,9 +23,7 @@ function generatecharacter() {
         case "Elf":
             generatednames =elfnames[generateRandomInteger(elfnames.length)-1];
             break;
-
     }
-
     document.getElementById("generaltclassrace").innerHTML = "Your character could be a/an " + generatedrace + " " + generatedclass + " whose name is " + generatednames;
 }
 
@@ -46,7 +45,34 @@ function genrandomnames() {
     document.getElementById("genrandomnames").innerHTML = text;
 }
 
+function genrandomstat() {
+    let stats = [statroll(),statroll(),statroll(),statroll(),statroll(),statroll()];
+    let str = statroll();
+    let dex = statroll();
+    let con = statroll();
+    let int = statroll();
+    let wis = statroll();
+    let cha = statroll();
+    /*console.log (str + " " + dex + " " + con  + " " + int + " " + wis + " " + cha);*/
+console.log(stats)
+}
 
+function statroll(){
+    let stat = [0,0,0,0];
+    for (let i = 0; i < 4; i++) {
+        stat[i] = generateRandomInteger(6);
+    }
+
+let sum = 0;
+    for (let i = 0; i < stat.length; i++){
+        sum += stat[i];
+
+    }
+
+let min = Math.min.apply(Math, stat) ;
+    sum = sum - min;
+    return sum;
+}
 
 /*Generator Function*/
 function generateRandomInteger(max) {
