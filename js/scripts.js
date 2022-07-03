ce8a75f86ace7e09ea4e88ea32fbccc6fc760dd6
Sárdi András <sardi.andras@gmail.com>
2022-07-03 17:41:56 +0200

    classrace

--- js/scripts.js
+++ js/scripts.js
@@ -4,8 +4,21 @@ const elfnames = ["Na Io","Qia'Lenda","Umbra'Dal","Vala Nailo"]
 const allnames = humannames.concat(dwarfnames,elfnames);
 const generatednames = ["1","2","3","4","5","6","7","8","9","10"];
 
-const dndclass = ["Artificer","Barbarian","Bard","Blood Hunter","Cleric","Druid","Fighter","Monk","Paladin","Ranger","Rogue","HSorcerer","Warlock","Wizard"];
+const dndclass = ["Artificer","Barbarian","Bard","Blood Hunter","Cleric","Druid","Fighter","Monk","Paladin","Ranger","Rogue","Sorcerer","Warlock","Wizard"];
 const dndrace = ["Dragonborn","Dwarf","Elf","Gnome","Half-Elf","Half-Orc","Halfling","Human","Tiefling","Aarakocra","Aasimar","Fairy","Firbolg","Genasi","Gith","Goliath","Harengon","Kenku","Locathah","Owlin","Tabaxi","Tortle","Triton","Verdan","Bugbear","Centaur","Goblin","Grung","Hobgoblin","Kobold","Lizardfolk","Minotaur","Orc","Yuan-Ti","Pureblood","Autognome","Dhamphir","Giff","Hadozee","Hexblood","Fairy","Kender","Owlfolk","Plasmoid","Rabbitfolk","Reborn","Revenant","Thri-Kreen","Eberron","Changeling","Kalashtar","Shifter","Warforged","Aetherborn","Aven","Khenra","Kor","Merfolk","Naga","Siren","Vampire","Dhamphir","Hexblood","Reborn","Ravnica","Loxodon","Simic","Hybrid","Vedalken","Viashino","Theros","Leonin","Satyr"];
+
+function generatecharacter() {
+    let text ="";
+    let generatedclass ="";
+    let generatedrace ="";
+
+    generatedclass = dndclass[generateRandomInteger(dndclass.length)-1];
+
+    generatedrace = dndrace[generateRandomInteger(dndrace.length)-1];
+
+text += "A te karaktered lehetne egy" + " " + generatedrace + " " + generatedclass + "!";
+console.log(text);
+}
 
 function generate() {
     let text = "";
